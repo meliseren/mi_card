fb57727df3d9e109ddaefb7ca781b948ebf79ead
Kübra Melis Eren <76619073+meliseren@users.noreply.github.com>
2024-05-05 17:38:38 +0300

    test

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mi_card/screens/home_screens.dart';
+
+void main() {
+  runApp(const HomeScreen());
+}
---
+++ lib/screens/home_screens.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(71, 51, 255, 1),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.black,
+                backgroundImage: AssetImage(
+                  'assets/images/melis.jpeg',
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Melis Eren',
+                style: GoogleFonts.pacifico(
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              Container(height: 1.0, width: 200.0, color: Colors.white),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Card(
+                margin: EdgeInsets.symmetric(horizontal: 40.0),
+                child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        size: 32.0,
+                        color: Color.fromRGBO(71, 51, 255, 1),
+                      ),
+                      title: Text(
+                        '+90 533 731 4999',
+                        style: TextStyle(color: Colors.black, fontSize: 20.0),
+                      ),
+                    )),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Card(
+                margin: EdgeInsets.symmetric(horizontal: 40.0),
+                child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        size: 32.0,
+                        color: Color.fromRGBO(71, 51, 255, 1),
+                      ),
+                      title: Text(
+                        'kubrameliseren@icloud.com',
+                        style: TextStyle(color: Colors.black, fontSize: 20.0),
+                      ),
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
